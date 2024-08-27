3a963a20de65d8195c9bcf48e9ae49fdb029ec28
skuukzky <ryujinkun34@gmail.com>
2024-08-27 14:03:22 +0700

    implement search

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,9 +2,10 @@ import { PrismaClient } from "@prisma/client";
 import AddProduct from "./addProduct";
 import DeleteProduct from "./deleteProduct";
 import UpdateProduct from "./updateProduct";
+import Search from "./search";
 const prisma = new PrismaClient();
 
-const getProducts = async () => {
+const getProducts = async (query: string, page: number) => {
   const res = await prisma.product.findMany({
     select: {
       id: true,
@@ -12,6 +13,16 @@ const getProducts = async () => {
       price: true,
       brandId: true,
       brand: true,
+    },
+    where: {
+      OR: [
+        {
+          title: {
+            contains: query,
+            mode: "insensitive",
+          },
+        },
+      ],
     },
   });
   return res;
@@ -22,12 +33,23 @@ const getBrands = async () => {
   return res;
 };
 
-const ProductPage = async () => {
-  const [products, brands] = await Promise.all([getProducts(), getBrands()]);
+const ProductPage = async ({
+  searchParams,
+}: {
+  searchParams?: { query?: string; page?: string };
+}) => {
+  const query = searchParams?.query || "";
+  const page = searchParams?.page || 1;
+
+  const [products, brands] = await Promise.all([
+    getProducts(query, Number(page)),
+    getBrands(),
+  ]);
 
   return (
     <div>
-      <div className="mb-2">
+      <div className="mb-2 flex items-center gap-2">
+        <Search />
         <AddProduct brands={brands} />
       </div>
       <table className="table w-full">
